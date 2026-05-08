0de174bf69232fa30da0f25860ce3a1f639ebe66
sonal0409 <mittal.sonal04@gmail.com>
2026-05-08 20:10:47 +0530

    done

--- app.py
+++ app.py
@@ -2,8 +2,6 @@
 # creates a simple web server using Flask that returns "AI DevOps Bootcamp is awsome!" when the root URL is accessed.
 # app should run on port 5000 
 # host on 0.0.0.0
-# the code should also return "AI with DevOps is the future!" when the /future URL is accessed, along with a 200 status code.
-
 from flask import Flask
 app = Flask(__name__)
 
@@ -13,5 +11,6 @@ def home():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+
 
 
